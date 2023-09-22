905d5f81ffe11e27dd5b1d95103c611d9a7d57d9
tigeet <tigeeet@gmail.com>
2023-09-22 23:15:55 +0300

    fix: fixed issue url comparasion

--- resources/scripts/link.js
+++ resources/scripts/link.js
@@ -1,12 +1,25 @@
 const linkSelector = ".nav__link";
 const linkModifier = "link-active";
 
+const getResource = (pathname) => pathname.split("/").at(-1);
+
+const isIndex = (pathname) => {
+  const resource = getResource(pathname);
+  return resource === "" || resource === "index.html";
+};
+
+const areUrlsEqual = (at, pathname) => {
+  if (isIndex(at) && isIndex(pathname)) return true;
+  return at === pathname;
+};
+
 (function () {
-  const url = window.location.href;
+  const at = window.location.pathname;
 
   const links = document.querySelectorAll(linkSelector);
+
   for (const link of links) {
-    if (link.href !== url) continue;
+    if (!areUrlsEqual(at, link.pathname)) continue;
 
     link.classList.add(linkModifier);
   }
